0c549b1bf384b512914a7b90c7bb2c27fabb378a
rafaelpramathana0610 <117884225+rafaelpramathana0610@users.noreply.github.com>
2023-01-03 12:18:59 +0700

    Add files via upload

---
+++ soal1.py
@@ -0,0 +1,135 @@
+print("****** Kredit Keaktifan Mahasiswa ******")
+print("Student Activities Credit")
+print("1. Menambahkan Kegiatan")
+print("2. Menampilkan Jumlah Poin")
+print("3. Keluar")
+print("---------------------------")
+
+pilihan = int(input("Silahkan masukan pilihan yang Anda: "))
+
+if pilihan == 1:
+    Nama = input("Nama Kegiatan: ")
+    Tanggal = input("Tanggal Kegiatan: ")
+
+print("Pilihan Kategori Kegiatan: ")
+print("- Prestasi")
+print("- Organisasi")
+print("- Kepanitiaan")
+print("- Rekognisi")
+
+Organisasi = input("Masukkan Kategori Kegiatan: ")
+
+if Organisasi == Organisasi:
+    print("Kegiatan berhasil ditambahkan.")
+
+print("****** Kredit Keaktifan Mahasiswa ******")
+print("Student Activities Credit")
+print("1. Menambahkan Kegiatan")
+print("2. Menampilkan Jumlah Poin")
+print("3. Keluar")
+print("---------------------------")
+
+pilihan = int(input("Silahkan masukan pilihan yang Anda: "))
+
+if pilihan == 1:
+    Nama = input("Nama Kegiatan: ")
+    Tanggal = input("Tanggal Kegiatan: ")
+
+print("Pilihan Kategori Kegiatan: ")
+print("- Prestasi")
+print("- Organisasi")
+print("- Kepanitiaan")
+print("- Rekognisi")
+
+Kepanitiaan = input("Masukkan Kategori Kegiatan: ")
+
+if Kepanitiaan == Kepanitiaan:
+    print("Kegiatan berhasil ditambahkan.")
+
+print("****** Kredit Keaktifan Mahasiswa ******")
+print("Student Activities Credit")
+print("1. Menambahkan Kegiatan")
+print("2. Menampilkan Jumlah Poin")
+print("3. Keluar")
+print("---------------------------")
+
+pilihan = int(input("Silahkan masukan pilihan yang Anda: "))
+
+if pilihan == 1:
+    Nama = input("Nama Kegiatan: ")
+    Tanggal = input("Tanggal Kegiatan: ")
+
+print("Pilihan Kategori Kegiatan: ")
+print("- Prestasi")
+print("- Organisasi")
+print("- Kepanitiaan")
+print("- Rekognisi")
+
+Prestasi = input("Masukkan Kategori Kegiatan: ")
+
+if Prestasi == Prestasi:
+    print("Kegiatan berhasil ditambahkan.")
+
+print("****** Kredit Keaktifan Mahasiswa ******")
+print("Student Activities Credit")
+print("1. Menambahkan Kegiatan")
+print("2. Menampilkan Jumlah Poin")
+print("3. Keluar")
+print("---------------------------")
+
+pilihan = int(input("Silahkan masukan pilihan yang Anda: "))
+
+if pilihan == 1:
+    Nama = input("Nama Kegiatan: ")
+    Tanggal = input("Tanggal Kegiatan: ")
+
+print("Pilihan Kategori Kegiatan: ")
+print("- Prestasi")
+print("- Organisasi")
+print("- Kepanitiaan")
+print("- Rekognisi")
+
+Rekognisi = input("Masukkan Kategori Kegiatan: ")
+
+if Rekognisi == Rekognisi:
+    print("Kegiatan berhasil ditambahkan.")
+
+print("****** Kredit Keaktifan Mahasiswa ******")
+print("Student Activities Credit")
+print("1. Menambahkan Kegiatan")
+print("2. Menampilkan Jumlah Poin")
+print("3. Keluar")
+print("---------------------------")
+
+pilihan = int(input("Silahkan masukan pilihan yang Anda: "))
+
+if pilihan == 1:
+    Nama = input("Nama Kegiatan: ")
+    Tanggal = input("Tanggal Kegiatan: ")
+
+print("Pilihan Kategori Kegiatan: ")
+print("- Prestasi")
+print("- Organisasi")
+print("- Kepanitiaan")
+print("- Rekognisi")
+
+Kepanitiaan = input("Masukkan Kategori Kegiatan: ")
+
+if Kepanitiaan == Kepanitiaan:
+    print("Kegiatan berhasil ditambahkan.")
+
+print("****** Kredit Keaktifan Mahasiswa ******")
+print("Student Activities Credit")
+print("1. Menambahkan Kegiatan")
+print("2. Menampilkan Jumlah Poin")
+print("3. Keluar")
+print("---------------------------")
+
+pilihan = int(input("Silahkan masukan pilihan yang Anda: "))
+
+if pilihan == 3:
+    print("Sistem Berhenti... ")
+
+
+
+
